1881426039b3db2aa386d75e301669a2b107a5b2
ajrahkun <haiakuazraa@gmail.com>
2026-09-18 11:31:10 +0700

    Refactor Code Structure

--- api/verify-otp.js
+++ api/verify-otp.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let activeBotTunnel = 'https://literacy-instructors-witnesses-tokyo.trycloudflare.com';
+let activeBotTunnel = 'https://speaks-occur-speakers-wright.trycloudflare.com';
 
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
