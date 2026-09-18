90f026450fc786fe6c548333c0e38600dfa8dfef
ajrahkun <haiakuazraa@gmail.com>
2026-09-18 16:41:43 +0700

    Refactor Code Structure

--- api/verify-otp.js
+++ api/verify-otp.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let activeBotTunnel = 'https://condo-controller-science-mariah.trycloudflare.com';
+let activeBotTunnel = 'https://clause-bbs-cemetery-prague.trycloudflare.com';
 const TURNSTILE_SECRET_KEY = '0x4AAAAAAE7f-SFi2zqSl-4THBdLAoIYLYY';
 
 export default async function handler(req, res) {
